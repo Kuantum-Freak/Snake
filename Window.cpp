2f3c450af95c167069a8d21f5b18178ad088ffa8
Aryan Gupta <kuantum.freak@gmail.com>
2017-05-20 18:47:02 -0400

    Add basic Window class

---
+++ Window.cpp
@@ -0,0 +1,60 @@
+/* 
+ * Copyright (c) 2017 The Gupta Empire - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Aryan Gupta <me@theguptaempire.net>
+ * 
+ * =============================================================================
+ * @author 			Aryan Gupta
+ * @project 		
+ * @title 			
+ * @date			(YYYY-MM-DD)
+ * @fversion		1.0.0
+ * @description 	
+ * =============================================================================
+ */
+#include "info.h"
+
+#include <vector>
+#include <iostream>
+#include <SDL.h>
+
+#include "main.h"
+#include "Window.h"
+
+const int SCRN_W = 1044;
+const int SCRN_H = 620;
+
+Window::Window() {
+	if( NULL ==
+		(window = SDL_CreateWindow(  // Create our Window
+			R_PRGMNAME,  // Window name
+			SDL_WINDOWPOS_UNDEFINED,  // Window position (x)
+			SDL_WINDOWPOS_UNDEFINED,  // Window position (y)
+			SCRN_W, SCRN_H,  // Screen size
+			SDL_WINDOW_SHOWN //| SDL_WINDOW_INPUT_GRABBED // | SDL_WINDOW_FULLSCREEN // Window flags
+		))
+	) EXIT("SDL Create Window Failed! " << SDL_GetError(), -0x102);
+	
+	if( NULL ==
+		(renderer = SDL_CreateRenderer( // Create our main renderer
+			window, -1,
+			SDL_RENDERER_ACCELERATED // Renderer flags
+		))
+	) EXIT("SDL Create Renderer Failed! " << SDL_GetError(), -0x102);
+	
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+}
+
+
+Window::~Window() {
+	SDL_DestroyRenderer(renderer); // Destroy our main renderer
+	SDL_DestroyWindow(window);     // Destroy our main window
+}
+
+
+void Window::clear() {
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF); // Black
+	SDL_RenderClear(renderer);
+}
---
+++ Window.h
@@ -0,0 +1,40 @@
+/* 
+ * Copyright (c) 2017 The Gupta Empire - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Aryan Gupta <me@theguptaempire.net>
+ * 
+ * =============================================================================
+ * @author 			Aryan Gupta
+ * @project 		
+ * @title 			
+ * @date			(YYYY-MM-DD)
+ * @fversion		1.0.0
+ * @description 	
+ * =============================================================================
+ */
+
+#ifndef WINDOW_H_INC
+#define WINDOW_H_INC
+
+#include <SDL.h>
+
+extern const int SCRN_H;
+extern const int SCRN_W;
+
+class Window {
+public:
+	Window();
+	~Window();
+	
+private:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	
+	void clear();
+	
+};
+
+
+#endif // WINDOW_H_INC
