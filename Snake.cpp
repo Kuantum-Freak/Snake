b2a94178b0746938406eeb0274756c5ac4df2865
Aryan Gupta <kuantum.freak@gmail.com>
2017-05-20 18:07:48 -0400

    Snake::move() FUNC

--- Snake.cpp
+++ Snake.cpp
@@ -16,4 +16,27 @@
  */
 #include "info.h"
 
-#include ".\h\main.h"
+#include <SDL.h>
+
+#include "main.h"
+#include "Snake.h"
+
+Snake::Snake() {
+	
+}
+
+Snake::~Snake() {
+	
+}
+
+void Snake::move() {
+	std::assert(snake[snake.size() - 1]->dir == headDir); // make sure that the direction of the head is correct
+	
+	++snake[snake.size() - 1]->length; // increment the length of the head segment
+	
+	--snake[0]->length; // decrement the tail segment
+	if(snake[0]->length == 0) { // if there is no tail segment remove it
+		delete snake[0];
+		snake.erase(0);
+	}
+}
